63b85a75096bc09fcb68cc87129b41b5875772ce
Mark Dierolf <mdierolf@cloudquote.io>
2025-06-23 17:36:43 -0700

    Converted to an ES module; removed babel; fixed a bug where multiple up and down keypresses would wipe the history

--- test/helpers/readline.js
+++ test/helpers/readline.js
@@ -1,39 +1,39 @@
-var EventEmitter = require('events').EventEmitter;
-var sinon = require('sinon');
-var util = require('util');
-var _ = require('lodash');
+import { EventEmitter } from 'events'
+import sinon from 'sinon'
+import util from 'util'
+import _ from 'lodash'
 
-var stub = {};
+const stub = {}
 
 _.assign(stub, {
-  write: sinon.stub().returns(stub),
-  moveCursor: sinon.stub().returns(stub),
-  setPrompt: sinon.stub().returns(stub),
-  close: sinon.stub().returns(stub),
-  pause: sinon.stub().returns(stub),
-  resume: sinon.stub().returns(stub),
-  _getCursorPos: sinon.stub().returns({
-    cols: 0,
-    rows: 0
-  }),
-  output: {
-    end: sinon.stub(),
-    mute: sinon.stub(),
-    unmute: sinon.stub(),
-    __raw__: '',
-    write: function(str) {
-      this.__raw__ += str;
-    }
+ write: sinon.stub().returns(stub),
+ moveCursor: sinon.stub().returns(stub),
+ setPrompt: sinon.stub().returns(stub),
+ close: sinon.stub().returns(stub),
+ pause: sinon.stub().returns(stub),
+ resume: sinon.stub().returns(stub),
+ _getCursorPos: sinon.stub().returns({
+  cols: 0,
+  rows: 0
+ }),
+ output: {
+  end: sinon.stub(),
+  mute: sinon.stub(),
+  unmute: sinon.stub(),
+  __raw__: '',
+  write: function(str) {
+   this.__raw__ += str
   }
-});
+ }
+})
 
-var ReadlineStub = function() {
-  this.line = '';
-  this.input = new EventEmitter();
-  EventEmitter.apply(this, arguments);
-};
+const ReadlineStub = function() {
+ this.line = ''
+ this.input = new EventEmitter()
+ EventEmitter.apply(this, arguments)
+}
 
-util.inherits(ReadlineStub, EventEmitter);
-_.assign(ReadlineStub.prototype, stub);
+util.inherits(ReadlineStub, EventEmitter)
+_.assign(ReadlineStub.prototype, stub)
 
-module.exports = ReadlineStub;
+export default ReadlineStub
--- test/index.js
+++ test/index.js
@@ -1,127 +1,130 @@
-/* globals Promise */
-
 /* eslint-disable no-unused-vars */
 
-var assert = require('assert')
-var ReadlineStub = require('./helpers/readline')
-var Prompt = require('..')
+import assert from 'assert'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import ReadlineStub from './helpers/readline.js'
+import Prompt from '../index.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 describe('inquirer-command-prompt', function () {
 
-  var prompt
-  var rl
-  var availableCommands
-  var promiseForAnswer
+ var prompt
+ var rl
+ var availableCommands
+ var promiseForAnswer
 
-  describe('auto-complete', function () {
+ describe('auto-complete', function () {
 
-    beforeEach(function () {
-      availableCommands = ['foo', 'bar', 'bum']
-      rl = new ReadlineStub()
-    })
-
-    it('should returns the expected word if that is partially typed', function () {
-      prompt = new Prompt({
-        message: '>',
-        name: 'name',
-        autoCompletion: availableCommands,
-        context: 0
-      }, rl)
-
-      promiseForAnswer = getPromiseForAnswer()
-
-      type('f')
-      tab()
-      enter()
-
-      return promiseForAnswer.then(function () {
-        assert(rl.line === 'foo')
-      })
-    })
-
-    it('should returns the type word if tab not pressed', function () {
-      prompt = new Prompt({
-        message: '>',
-        name: 'name',
-        // autoCompletion: availableCommands,
-        context: 0
-      }, rl)
-
-      promiseForAnswer = getPromiseForAnswer()
-
-      type('hello')
-      enter()
-
-      return promiseForAnswer.then(function (answer) {
-        assert(rl.line === 'hello')
-      })
-    })
-
-    it('should returns the type word if tab pressed but no matches', function () {
-      prompt = new Prompt({
-        message: '>',
-        name: 'name',
-        // autoCompletion: availableCommands,
-        context: 0
-      }, rl)
-
-      promiseForAnswer = getPromiseForAnswer()
-
-      type('zu')
-      tab()
-      enter()
-
-      return promiseForAnswer.then(function (answer) {
-        assert(rl.line === 'zu')
-      })
-    })
-
+  beforeEach(function () {
+   availableCommands = ['foo', 'bar', 'bum']
+   rl = new ReadlineStub()
   })
 
+  it('should returns the expected word if that is partially typed', function () {
+   prompt = new Prompt({
+    message: '>',
+    name: 'name',
+    autoCompletion: availableCommands,
+    context: 0
+   }, rl)
 
-  describe('history', function () {
+   promiseForAnswer = getPromiseForAnswer()
 
-    // to be done
+   type('f')
+   tab()
+   enter()
 
+   return promiseForAnswer.then(function () {
+    assert(rl.line === 'foo')
+   })
   })
 
-  function getPromiseForAnswer() {
-    return prompt.run()
-  }
+  it('should returns the type word if tab not pressed', function () {
+   prompt = new Prompt({
+    message: '>',
+    name: 'name',
+    // autoCompletion: availableCommands,
+    context: 0
+   }, rl)
 
-  function typeNonChar() {
-    rl.input.emit('keypress', '', {
-      name: 'shift'
-    })
-  }
+   promiseForAnswer = getPromiseForAnswer()
 
-  function type(word) {
-    word.split('').forEach(function (char) {
-      rl.line = rl.line + char
-      rl.input.emit('keypress', char)
-    })
-  }
+   type('hello')
+   enter()
 
-  function moveDown() {
-    rl.input.emit('keypress', '', {
-      name: 'down'
-    })
-  }
+   return promiseForAnswer.then(function (answer) {
+    assert(rl.line === 'hello')
+   })
+  })
 
-  function moveUp() {
-    rl.input.emit('keypress', '', {
-      name: 'up'
-    })
-  }
+  it('should returns the type word if tab pressed but no matches', function () {
+   prompt = new Prompt({
+    message: '>',
+    name: 'name',
+    // autoCompletion: availableCommands,
+    context: 0
+   }, rl)
 
-  function enter() {
-    rl.emit('line')
-  }
+   promiseForAnswer = getPromiseForAnswer()
 
-  function tab() {
-    rl.input.emit('keypress', '', {
-      name: 'tab'
-    })
-  }
+   type('zu')
+   tab()
+   enter()
+
+   return promiseForAnswer.then(function (answer) {
+    assert(rl.line === 'zu')
+   })
+  })
+
+ })
+
+
+ describe('history', function () {
+
+  // to be done
+
+ })
+
+ function getPromiseForAnswer() {
+  return prompt.run()
+ }
+
+ function typeNonChar() {
+  rl.input.emit('keypress', '', {
+   name: 'shift'
+  })
+ }
+
+ function type(word) {
+  word.split('').forEach(function (char) {
+   rl.line = rl.line + char
+   rl.input.emit('keypress', char)
+  })
+ }
+
+ function moveDown() {
+  rl.input.emit('keypress', '', {
+   name: 'down'
+  })
+ }
+
+ function moveUp() {
+  rl.input.emit('keypress', '', {
+   name: 'up'
+  })
+ }
+
+ function enter() {
+  rl.emit('line')
+ }
+
+ function tab() {
+  rl.input.emit('keypress', '', {
+   name: 'tab'
+  })
+ }
 
 })
